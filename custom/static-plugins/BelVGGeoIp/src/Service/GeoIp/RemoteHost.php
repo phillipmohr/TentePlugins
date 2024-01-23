3c301d2fe981d126a8b2c062034f029674b7ed4d
Nikolay Razumec <nikolayrazumec@gmail.com>
2024-01-24 00:56:49 +0300

    [SFN-6] add services geoIp, get Ip, getCountry data by ip

---
+++ custom/static-plugins/BelVGGeoIp/src/Service/GeoIp/RemoteHost.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace BelVGGeoIp\Service\GeoIp;
+
+use GeoIp2\ProviderInterface;
+use GeoIp2\WebService\Client;
+
+/**
+ *
+ */
+class RemoteHost extends GeoIpProvider
+{
+    /**
+     *
+     */
+    protected const ACCOUNT_ID = 568511;
+    /**
+     *
+     */
+    protected const LICENSE_KEY = 'kBqrf8F3xbkLYmj0';
+    /**
+     *
+     */
+    protected const LOCALES = ['en'];
+    /**
+     *
+     */
+    protected const OPTIONS = ['host' => 'geolite.info'];
+
+    /**
+     * @return int
+     */
+    protected function getAccountId(): int
+    {
+        return self::ACCOUNT_ID;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getLicenseKey(): string
+    {
+        return self::LICENSE_KEY;
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getLocales(): array
+    {
+        return self::LOCALES;
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getOptions(): array
+    {
+        return self::OPTIONS;
+    }
+
+    /**
+     * @return ProviderInterface|null
+     */
+    public function init(): ?ProviderInterface
+    {
+        return $this->initСlient();
+    }
+
+    /**
+     * @return ProviderInterface|null
+     */
+    public function initСlient(): ?ProviderInterface
+    {
+        return new Client($this->getAccountId(), $this->getLicenseKey(), $this->getLocales(), $this->getOptions());
+    }
+}
--- custom/static-plugins/BelVGGeoIp/src/Subscriber/GeoIpSubscriber.php
+++ custom/static-plugins/BelVGGeoIp/src/Subscriber/GeoIpSubscriber.php
@@ -2,13 +2,32 @@
 
 namespace BelVGGeoIp\Subscriber;
 
+use BelVGGeoIp\Service\GeoData;
+use BelVGGeoIp\Service\IpAddress;
 use Shopware\Core\Framework\Struct\ArrayEntity;
 use Shopware\Storefront\Event\StorefrontRenderEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 
+/**
+ *
+ */
 class GeoIpSubscriber implements EventSubscriberInterface
 {
+
+    /**
+     * @param RequestStack $request
+     * @param GeoData $geoData
+     */
+    public function __construct(protected RequestStack $request,
+                                protected GeoData      $geoData)
+    {
+    }
+
+    /**
+     * @return array[]
+     */
     public static function getSubscribedEvents(): array
     {
         return [
@@ -23,12 +42,11 @@ class GeoIpSubscriber implements EventSubscriberInterface
      */
     public function addStoreSwitcherInfo(StorefrontRenderEvent $event): void
     {
-
-
+        $ip = IpAddress::getIpAddress();
+        $country = $this->geoData->getCountry($ip);
         $event->setParameter('geoIP', new ArrayEntity([
-
-            'currentCountry' => [1, 2, 3, 4, 5]
-
+            'country' => $country,
+            'countryIsoCode' => $country?->country->isoCode
         ]));
     }
 }
